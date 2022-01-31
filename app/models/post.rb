e6dffc76efd65fe702fde7eb51e24b16a116f762
root <root@LAPTOP-78CKJOIP.localdomain>
2022-01-31 05:38:16 -0600

    commit tree b7bd20149f21d73578fd3e072b2306c09a4fac30

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { :minimum => 250 }
+    validates :summary, length: { :maximum => 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :no_true_facts_in_title 
+    
+    def no_true_facts_in_title
+        if title == "True Facts"
+            errors.add(:title, "Nope!")
+        end
+    end
+
 end
